0267c99edda31081b1025cacbb4f681bc580f2ab
Nicolasr3 <nicolasrussi3@gmail.com>
2023-02-27 11:37:05 +0000

    update variants.*

--- samd/1.0.0/variants/misterino_zero/variant.h
+++ samd/1.0.0/variants/misterino_zero/variant.h
@@ -16,8 +16,8 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#ifndef _VARIANT_ARDUINO_ZERO_
-#define _VARIANT_ARDUINO_ZERO_
+#ifndef _VARIANT_MISTERINO_ZERO_
+#define _VARIANT_MISTERINO_ZERO_
 
 // The definitions here needs a SAMD core >=1.6.10
 #define ARDUINO_SAMD_VARIANT_COMPLIANCE 10610
@@ -59,7 +59,7 @@ extern "C"
 extern "C" unsigned int PINCOUNT_fn();
 #endif
 #define PINS_COUNT           (PINCOUNT_fn())
-#define NUM_DIGITAL_PINS     (13u)
+#define NUM_DIGITAL_PINS     (10u)
 #define NUM_ANALOG_INPUTS    (4u)
 #define NUM_ANALOG_OUTPUTS   (1u)
 #define analogInputToDigitalPin(p)  ((p < 6u) ? (p) + 14u : -1)
@@ -82,18 +82,18 @@ extern "C" unsigned int PINCOUNT_fn();
 // #define digitalPinToTimer(P)
 
 // LEDs
-#define PIN_LED_18           (18u)
-#define PIN_LED              PIN_LED_18
-#define LED_BUILTIN          PIN_LED_18
+#define PIN_LED_20           (20u)
+#define PIN_LED              PIN_LED_20
+#define LED_BUILTIN          PIN_LED_20
 
 /*
  * Analog pins
  */
-#define PIN_A0               (12ul)
-#define PIN_A1               (13ul)
-#define PIN_A2               (14ul)
-#define PIN_A3               (15ul)
-#define PIN_DAC0             (12ul)
+#define PIN_A0               (10ul)
+#define PIN_A1               (11ul)
+#define PIN_A2               (12ul)
+#define PIN_A3               (13ul)
+#define PIN_DAC0             (10ul)
 
 static const uint8_t A0   = PIN_A0;
 static const uint8_t A1   = PIN_A1;
@@ -112,14 +112,32 @@ static const uint8_t DAC0 = PIN_DAC0;
 #define PAD_SERIAL1_RX       (SERCOM_RX_PAD_3)
 
 /*
+ * SPI Interfaces
+ */
+#define SPI_INTERFACES_COUNT 1
+
+#define PIN_SPI_MISO         (16u)
+#define PIN_SPI_MOSI         (17u)
+#define PIN_SPI_SCK          (18u)
+#define PERIPH_SPI           sercom1
+#define PAD_SPI_TX           SPI_PAD_2_SCK_3
+#define PAD_SPI_RX           SERCOM_RX_PAD_0
+
+static const uint8_t SS	  = PIN_A2 ;	// SERCOM4 last PAD is present on A2 but HW SS isn't used. Set here only for reference.
+static const uint8_t MOSI = PIN_SPI_MOSI ;
+static const uint8_t MISO = PIN_SPI_MISO ;
+static const uint8_t SCK  = PIN_SPI_SCK ;
+
+
+/*
  * Wire Interfaces
  */
 #define WIRE_INTERFACES_COUNT 1
 
-#define PIN_WIRE_SDA         (16u)
-#define PIN_WIRE_SCL         (17u)
+#define PIN_WIRE_SDA         (14u)
+#define PIN_WIRE_SCL         (15u)
 #define PERIPH_WIRE          sercom0
-#define WIRE_IT_HANDLER      SERCOM3_Handler
+#define WIRE_IT_HANDLER      SERCOM0_Handler
 
 static const uint8_t SDA = PIN_WIRE_SDA;
 static const uint8_t SCL = PIN_WIRE_SCL;
@@ -127,9 +145,9 @@ static const uint8_t SCL = PIN_WIRE_SCL;
 /*
  * USB
  */
-#define PIN_USB_HOST_ENABLE (19ul)
-#define PIN_USB_DM          (20ul)
-#define PIN_USB_DP          (21ul)
+#define PIN_USB_HOST_ENABLE (20ul)
+#define PIN_USB_DM          (21ul)
+#define PIN_USB_DP          (22ul)
 
 /*
  * I2S Interfaces
@@ -192,10 +210,10 @@ unsigned int PINCOUNT_fn();
 // SERIAL_PORT_HARDWARE_OPEN  Hardware serial ports which are open for use.  Their RX & TX
 //                            pins are NOT connected to anything by default.
 #define SERIAL_PORT_USBVIRTUAL      SerialUSB
-#define SERIAL_PORT_MONITOR         Serial
+#define SERIAL_PORT_MONITOR         SerialUSB
 // Serial has no physical pins broken out, so it's not listed as HARDWARE port
 #define SERIAL_PORT_HARDWARE        Serial1
 #define SERIAL_PORT_HARDWARE_OPEN   Serial1
 
-#endif /* _VARIANT_ARDUINO_ZERO_ */
+#endif /* _VARIANT_MISTERINO_ZERO_ */
 
